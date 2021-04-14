5cb46a3150a553d896ad8d459a6db8f8798754e4
Etsetro <ronalds.palacis@outlook.com>
2021-04-15 00:10:23 +0300

    Frontend setup

---
+++ pages/index.page.tsx
@@ -0,0 +1,7 @@
+import "../styles/Home.module.scss";
+import Head from "next/head";
+import Wheel from "../components/Wheel.component";
+
+export default function Home() {
+  return <h1>Home</h1>;
+}
