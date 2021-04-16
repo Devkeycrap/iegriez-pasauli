c244c1e56dd011e688b4aa8fe2844e70f864caf6
Etsetro <ronalds.palacis@outlook.com>
2021-04-17 00:44:23 +0300

    Endpoint for quiz questions

--- pages/api/src/server.js
+++ pages/api/src/server.js
@@ -5,3 +5,16 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/quiz/:item/questions", (req, res) => {
+  fs.readFile("./src/public/rules.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(402).send(err);
+    } else {
+      const questions = JSON.parse(data)
+        ["items"].filter((item) => item.name == req.params.item)[0]
+        .quiz.map((item) => item.question);
+      res.status(200).json({ questions });
+    }
+  });
+});
