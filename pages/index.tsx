c8af11fd12df027ed837f9b4211bdf44c60383ac
Etsetro <ronalds.palacis@outlook.com>
2021-04-19 14:01:18 +0300

    Moved navbar to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,6 @@ import Wheel from "../components/Wheel.component";
 export default function Home() {
   return (
     <div className="home">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Sākums</Link>
-          </li>
-          <li>
-            <Link href="/about-game">Par spēli</Link>
-          </li>
-          <li>
-            <Link href="/about-us">Par mums</Link>
-          </li>
-        </ul>
-      </nav>
       <main>
         <h1>
           Vai zini, kas ir <span>aprites ekonomika</span>?
