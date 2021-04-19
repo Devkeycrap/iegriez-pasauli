4ac3461cd9cdb5cf3309be2a1edf9ceb0c0a2923
Etsetro <ronalds.palacis@outlook.com>
2021-04-19 13:49:06 +0300

    Finished endpoints for quiz

--- pages/api/src/server.js
+++ pages/api/src/server.js
@@ -8,31 +8,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/quiz/:item/questions", (req, res) => {
+  // Get data from JSON and return questions
   fs.readFile("./src/public/rules.json", "utf8", (err, data) => {
     if (err) {
       res.status(402).send(err);
     } else {
       const questions = JSON.parse(data)
         ["items"].filter((item) => item.name == req.params.item)[0]
-        .quiz.map((item) => item.question);
+        ["quiz"].map((item) => item.question);
       res.status(200).json({ questions });
     }
   });
 });
 
 app.post("/quiz/:item/answer/:id", (req, res) => {
+  // Get data from JSON and validate an answer
   fs.readFile("./src/public/rules.json", "utf8", (err, data) => {
     if (err) {
       res.status(402).send(err);
     } else {
       const answer = JSON.parse(data)["items"].filter(
         (item) => item.name == req.params.item
-      )[0].quiz[req.params.id].answer;
+      )[0]["quiz"][req.params.id].answer;
       const userInput = req.body.answer;
-      console.log(req.body, req.params, req.query);
       const isCorrect = answer == userInput ? true : false;
-      console.log(isCorrect);
-      res.status(200).send({ isCorrect });
+      res.status(200).json({ isCorrect });
+    }
+  });
+});
     }
   });
 });
