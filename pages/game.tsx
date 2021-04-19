13b61e989a0a4a4796f7a41969dfb0c81b91722b
Etsetro <ronalds.palacis@outlook.com>
2021-04-19 14:00:28 +0300

    Updated game page

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,15 +1,58 @@
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import Questions from "../components/Questions.component";
+import Timer from "../components/Timer.component";
 import Wheel from "../components/Wheel.component";
+import dynamic from "next/dynamic";
 
 export default function Game() {
+  useEffect(() => {
+    setIsActive(true);
+    console.log("called");
+  }, []);
+  const [isActive, setIsActive] = useState(false);
   const [stage, setStage] = useState(1);
   const [gameObj, setGameObj] = useState(null);
+  const [gameEnded, setGameEnded] = useState(false);
+
+  const restartGame = () => {
+    setStage(1);
+    setGameEnded(false);
+    setIsActive(true);
+    setGameObj(null);
+  };
+
+  const Map = dynamic(() => import("../components/Map.component"), {
+    ssr: false,
+  });
 
   return (
     <div>
-      {stage === 1 && <Wheel setStage={setStage} setGameObj={setGameObj} />}
-      {stage === 2 && <Questions gameObj={gameObj} />}
+      {gameEnded && (
+        <div className="endscreen">
+          Time's up!
+          <Link href="/">
+            <button className="btn btn-neutral">Home</button>
+          </Link>
+          <Link href="/game">
+            <button onClick={restartGame} className="btn btn-green">
+              Play again
+            </button>
+          </Link>
+        </div>
+      )}
+      {!gameEnded && (
+        <div className="game">
+          <Timer
+            isActive={isActive}
+            setIsActive={setIsActive}
+            setGameEnded={setGameEnded}
+          />
+          {stage === 1 && <Wheel setStage={setStage} setGameObj={setGameObj} />}
+          {stage === 2 && <Questions gameObj={gameObj} setStage={setStage} />}
+          {stage === 3 && <Map gameObj={gameObj} />}
+        </div>
+      )}
     </div>
   );
 }
