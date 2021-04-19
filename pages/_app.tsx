2f9a25d6bcf5f2d13ea0c9e1b71cb299fce82066
Etsetro <ronalds.palacis@outlook.com>
2021-04-19 14:00:56 +0300

    App layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
+import Navbar from "../components/Navbar.component";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className="root">
       <Head>
         <title>Iegriez pasauli</title>
       </Head>
+      <Navbar />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
