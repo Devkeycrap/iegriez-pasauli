4f1e20b9e1b9b93fa59083ed77fccb3bd2b3d1e2
Etsetro <ronalds.palacis@outlook.com>
2021-04-22 14:28:07 +0300

    Added webpack svg config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "svg-url-loader",
+          options: {
+            limit: 10000,
+          },
+        },
+      ],
+    });
+    // Important: return the modified config
+    return config;
+  },
+};
