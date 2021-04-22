87c494166d6187d187a838652954efb4522d4992
Etsetro <ronalds.palacis@outlook.com>
2021-04-22 14:33:16 +0300

    Index: styles react approach

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
-import "../styles/Home.module.scss";
+import styles from "../styles/home.module.scss";
 import Link from "next/link";
 import Head from "next/head";
 import Wheel from "../components/Wheel.component";
 
 export default function Home() {
   return (
-    <div className="home">
-      <main>
+    <div className={styles.home}>
+      <div>
         <h1>
-          Vai zini, kas ir <span>aprites ekonomika</span>?
+          Vai zini, kas ir <br />
+          <span>aprites ekonomika</span>?
         </h1>
         <p>Pārbaudi savas zināšanas, spēlējot spēli “Iegriez pasauli”!</p>
         <Link href="game">
-          <button className="btn btn-orange">Sākt spēli</button>
+          <button className={styles["btn-orange"]}>Sākt spēli</button>
         </Link>
-      </main>
+      </div>
     </div>
   );
 }
