95356bba8f265250f12df2ab6c921155e0f6d29f
Devkeycrap <ronalds.palacis@outlook.com>
2021-04-26 15:10:09 +0300

    Game: moved wheel sectors

--- pages/game.tsx
+++ pages/game.tsx
@@ -5,18 +5,57 @@ import Timer from "../components/Timer.component";
 import Wheel from "../components/Wheel.component";
 import dynamic from "next/dynamic";
 import styles from "../styles/game.module.scss";
+import {
+  bananasPath,
+  tshirtPath,
+  burgerPath,
+  headphonesPath,
+  longboardPath,
+} from "../public/icons/paths";
 
-export default function Game() {
+export default function Game({ isActive, setIsActive }) {
   useEffect(() => {
     setIsActive(true);
     console.log("called");
   }, []);
 
-  const [isActive, setIsActive] = useState(false);
   const [stage, setStage] = useState(3);
   const [gameObj, setGameObj] = useState(null);
   const [gameEnded, setGameEnded] = useState(false);
   const [points, setPoints] = useState(0);
+  const [sectors, setSectors] = useState([
+    {
+      color: "#FFA52F",
+      image: bananasPath,
+      name: "Bananas",
+      displayName: "Banāni",
+    },
+    {
+      color: "#EBE1D1",
+      image: burgerPath,
+      name: "Hamburger",
+      displayName: "Hamburgers",
+    },
+    {
+      color: "#2C85A4",
+      image: headphonesPath,
+      name: "Headphones",
+      displayName: "Austiņas",
+    },
+    {
+      color: "#C3E5ED",
+      image: longboardPath,
+      name: "Longboard",
+      displayName: "Longbords",
+    },
+    {
+      color: "#FD6579",
+      image: tshirtPath,
+      imageColor: "",
+      name: "T-shirt",
+      displayName: "T-krekls",
+    },
+  ]);
 
   const restartGame = () => {
     setStage(1);
@@ -51,9 +90,18 @@ export default function Game() {
             setIsActive={setIsActive}
             setGameEnded={setGameEnded}
           />
-          {stage === 1 && <Wheel setStage={setStage} setGameObj={setGameObj} />}
-          {stage === 2 && <Questions gameObj={gameObj} setStage={setStage} />}
-          {stage === 3 && <Map gameObj={gameObj} />}
+          <div className={styles["stage-container"]}>
+            {stage === 1 && (
+              <Wheel
+                sectors={sectors}
+                setSectors={setSectors}
+                setStage={setStage}
+                setGameObj={setGameObj}
+              />
+            )}
+            {stage === 2 && <Questions gameObj={gameObj} setStage={setStage} />}
+            {stage === 3 && <Map setStage={setStage} gameObj={gameObj} />}
+          </div>
         </div>
       )}
     </div>
