812e1cbeba319af442efd3c26bce37687136dc9b
Devkeycrap <ronalds.palacis@outlook.com>
2021-04-30 19:45:03 +0300

    Server: added https only (commented out)

--- api/src/server.js
+++ api/src/server.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.enable("trust proxy"); //to detect if req.secure is true/false
+
+// //Block http requests. Only allow https requests
+// app.use(function (req, res, next) {
+//   if (req.headers["x-forwarded-proto"] !== "https") {
+//     return res.status(404).send("Not found");
+//   } else {
+//     next();
+//   }
+// });
 
 app.get("/quiz/:item/questions", (req, res) => {
   // Get data from JSON and return questions
