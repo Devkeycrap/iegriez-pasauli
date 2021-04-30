5aa5e1bd5ff41c47c69f6b52f91fc4da98d7c225
Devkeycrap <ronalds.palacis@outlook.com>
2021-05-01 00:07:44 +0300

    Game stage default set to 1

--- pages/game.tsx
+++ pages/game.tsx
@@ -16,7 +16,7 @@ import End from "../components/End.component";
 import IPoints from "../models/Points.model";
 
 export default function Game({ isActive, setIsActive }) {
-  const [stage, setStage] = useState(3);
+  const [stage, setStage] = useState(1);
   const [gameObj, setGameObj] = useState(null);
   const [gameEnded, setGameEnded] = useState(false);
   const [points, setPoints] = useState<IPoints>({
