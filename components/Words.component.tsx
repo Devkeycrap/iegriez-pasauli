ec5f417a1259f0e15828b67982f70c12e3550871
kristaps <kkrisapss.m@gmail.com>
2021-05-30 15:21:32 +0300

    Style improvments, Stage4

--- components/Words.component.tsx
+++ components/Words.component.tsx
@@ -1,8 +1,8 @@
-import styles from "../styles/words.module.scss"; // Import stylesheet for component
-import { Component } from "react"; // Import class Componenet from react
+import styles from "../styles/words.module.scss";
+import { Component } from "react";
 
 interface WordProps {
-  setGameEnded: (gameEnded: boolean) => void; // Import props from parent code
+  setGameEnded: (gameEnded: boolean) => void;
   setGameObj: (object: any) => void;
   setStage: (stage: number) => void;
   setPoints: (points: any) => void;
@@ -62,30 +62,30 @@ export default class Words extends Component<WordProps> {
   newObj(parent) {
     // Function for creating new componenet(word)
     if (this.state.objCount < this.state.maxObjCount) {
-      // Redundant if statment, seems to speed up the code, for no appearent reson, but if it works it works.
-      const obj = document.createElement("label"); // Create blank html <label/> element
+      const obj = document.createElement("label");
 
       obj.innerHTML = this.state.wordList.dictionary[
         Math.floor(Math.random() * this.state.wordList.dictionary.length)
-      ]; // Style <label/> element
+      ];
 
-      obj.style.left = Math.random() * 100 + "%"; // Set <label/> - horizontal  axis
-      obj.style.top = Math.random() * 100 + "%"; // Set <label/> - vertical    axis
-      obj.style.position = "absolute"; // Set <label/> - position
-      obj.style.fontSize = "16px"; // Set <label/> - font-size
+      obj.style.left = Math.random() * 100 + "%";
+      obj.style.top = Math.random() * 100 + "%";
       obj.style.width = "min-content";
+      obj.style.position = "absolute";
+      obj.style.fontSize = "16px";
+      
 
-      obj.classList.add("wordObj"); // /Style <label/> element
+      obj.classList.add("wordObj");
       this.setState({objCount: this.state.objCount+1});
       try {
-        parent.appendChild(obj); // Try adding the html <label/> to game canvas
+        parent.appendChild(obj);
       } catch {}
     }
     return;
   }
 
   newTick(elements) {
-    // Function for updating existing component(word) age/size.
+    // Function for updating existing component age
     for (let i = 0; i < elements.length; i++) {
       let element = elements[i];
       let wordSize = parseInt(
@@ -101,33 +101,30 @@ export default class Words extends Component<WordProps> {
   }
 
   clickEvent(e) {
-    // Function for handling click events inside of game canvas
-    let correct = false; // Variable for
+    let correct = false;
 
     this.state.wordList.correct.map((word, i) => {
-      // Compare if clicked on target has
-      if (e.target.innerHTML == word) return (correct = true); // innerHTML text, that is the correct text,
-    }); // by comparing it against this.state.wordList.correct list
+      if (e.target.innerHTML == word) return (correct = true);
+    });
 
     if (correct) {
-      // If compare loop returns, true,
-      this.setState({ points: this.state.points + 1 }); // Add 1 point to game counter
-      e.target.remove(); // Delete correctly clicked <label/>
+      this.setState({ points: this.state.points + 1 });
+      e.target.remove();
       this.setState({objCount: this.state.objCount-1});
     } else if (e.target.tagName == "LABEL") {
-      e.target.style.color = "#FD6579"; // else if clicked word is incorrect, we set the color to red
       e.target.style.pointerEvents = "none";
+      e.target.style.color = "#FD6579";
     }
   }
 
   componentDidMount() {
-    // Set inBrowser: true, for later validation NoSSR
     this.setState({ inBrowser: true });
 
     setTimeout(() => {
       clearInterval(this.mainthread);
       this.mainthread = setInterval(() => {
         this.newObj(document.getElementById("board"));
+        this.newTick(document.getElementsByClassName("wordObj"));
         this.newTick(document.getElementsByClassName("wordObj"));
       }, 300);
     }, 5000);
@@ -137,26 +134,28 @@ export default class Words extends Component<WordProps> {
       this.mainthread = setInterval(() => {
         this.newObj(document.getElementById("board"));
         this.newTick(document.getElementsByClassName("wordObj"));
+        this.newTick(document.getElementsByClassName("wordObj"));
       }, 250);
     }, 10000);
 
-      setTimeout(() => {
-        clearInterval(this.mainthread);
-        this.props.setGameObj(null);
-        if (this.props.sectors.length == 0) {
-          this.props.setPoints((points) => ({
-            ...points,
-            words: points.words + this.state.points,
-          }));
-          this.props.setGameEnded(true);
-        } else {
-          this.props.setPoints((points) => ({
-            ...points,
-            words: points.words + this.state.points,
-          }));
-          this.props.setStage(1);
-        }
-      }, 15000);
+      // Main 15 second timer (5 + 5 + 5)
+      // setTimeout(() => {
+      //   clearInterval(this.mainthread);
+      //   this.props.setGameObj(null);
+      //   if (this.props.sectors.length == 0) {
+      //     this.props.setPoints((points) => ({
+      //       ...points,
+      //       words: points.words + this.state.points,
+      //     }));
+      //     this.props.setGameEnded(true);
+      //   } else {
+      //     this.props.setPoints((points) => ({
+      //       ...points,
+      //       words: points.words + this.state.points,
+      //     }));
+      //     this.props.setStage(1);
+      //   }
+      // }, 15000);
   }
 
   componentWillUnmount() {
@@ -165,16 +164,32 @@ export default class Words extends Component<WordProps> {
 
   render() {
     if (!this.state.inBrowser) {
-      // Make sure if component loads in NoSSR mode
-      return null; // if doesnt, return null
+      return null;
     }
 
     return (
-      // Return html objects to be rendered
-      <div className={styles.backdrop} onClick={(e) => this.clickEvent(e)}>
-        <div className={styles["points-backdrop"]}><h2>+{this.state.points}</h2></div>
-        <div className={styles.canvas} id="board" />
-      </div>
+      <>
+        <div className={styles.backdrop} onClick={(e) => this.clickEvent(e)}>
+          <svg 
+            aria-hidden="true" 
+            focusable="false" 
+            data-prefix="fas" 
+            data-icon="globe-europe" 
+            role="img" 
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 496 512">
+            <path 
+              fill="currentColor" 
+              d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm200 248c0 22.5-3.9 44.2-10.8 64.4h-20.3c-4.3 0-8.4-1.7-11.4-4.8l-32-32.6c-4.5-4.6-4.5-12.1.1-16.7l12.5-12.5v-8.7c0-3-1.2-5.9-3.3-8l-9.4-9.4c-2.1-2.1-5-3.3-8-3.3h-16c-6.2 0-11.3-5.1-11.3-11.3 0-3 1.2-5.9 3.3-8l9.4-9.4c2.1-2.1 5-3.3 8-3.3h32c6.2 0 11.3-5.1 11.3-11.3v-9.4c0-6.2-5.1-11.3-11.3-11.3h-36.7c-8.8 0-16 7.2-16 16v4.5c0 6.9-4.4 13-10.9 15.2l-31.6 10.5c-3.3 1.1-5.5 4.1-5.5 7.6v2.2c0 4.4-3.6 8-8 8h-16c-4.4 0-8-3.6-8-8s-3.6-8-8-8H247c-3 0-5.8 1.7-7.2 4.4l-9.4 18.7c-2.7 5.4-8.2 8.8-14.3 8.8H194c-8.8 0-16-7.2-16-16V199c0-4.2 1.7-8.3 4.7-11.3l20.1-20.1c4.6-4.6 7.2-10.9 7.2-17.5 0-3.4 2.2-6.5 5.5-7.6l40-13.3c1.7-.6 3.2-1.5 4.4-2.7l26.8-26.8c2.1-2.1 3.3-5 3.3-8 0-6.2-5.1-11.3-11.3-11.3H258l-16 16v8c0 4.4-3.6 8-8 8h-16c-4.4 0-8-3.6-8-8v-20c0-2.5 1.2-4.9 3.2-6.4l28.9-21.7c1.9-.1 3.8-.3 5.7-.3C358.3 56 448 145.7 448 256zM130.1 149.1c0-3 1.2-5.9 3.3-8l25.4-25.4c2.1-2.1 5-3.3 8-3.3 6.2 0 11.3 5.1 11.3 11.3v16c0 3-1.2 5.9-3.3 8l-9.4 9.4c-2.1 2.1-5 3.3-8 3.3h-16c-6.2 0-11.3-5.1-11.3-11.3zm128 306.4v-7.1c0-8.8-7.2-16-16-16h-20.2c-10.8 0-26.7-5.3-35.4-11.8l-22.2-16.7c-11.5-8.6-18.2-22.1-18.2-36.4v-23.9c0-16 8.4-30.8 22.1-39l42.9-25.7c7.1-4.2 15.2-6.5 23.4-6.5h31.2c10.9 0 21.4 3.9 29.6 10.9l43.2 37.1h18.3c8.5 0 16.6 3.4 22.6 9.4l17.3 17.3c3.4 3.4 8.1 5.3 12.9 5.3H423c-32.4 58.9-93.8 99.5-164.9 103.1z"
+            ></path>
+          </svg>
+          <div className={styles["points-backdrop"]}><h2>+{this.state.points}</h2></div>
+          <div className={styles.canvas} id="board" />
+        </div>
+        {/* <div className={styles["overlay"]}>
+          
+        </div> */}
+      </>
     );
   }
 }
