01da356f61a246df21e399749942b44987a78ed3
Devkeycrap <ronalds.palacis@outlook.com>
2021-06-01 23:24:56 +0300

    Added redux provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,11 @@ import Head from "next/head";
 import { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 
-import { useSelector } from "react-redux";
-
+// Styles & animations
 import "../styles/globals.scss";
 
 // Components
 import Navbar from "../components/Navbar.component";
-import Spinner from "../components/Spinner.component";
 
 // Redux
 import { Provider } from "react-redux";
@@ -18,21 +16,24 @@ import store from "../store";
 // Main app component
 function MyApp({ Component, pageProps }) {
   const [isActive, setIsActive] = useState(false);
+
   return (
-    <div className="root">
-      <Head>
-        <title>Iegriez pasauli</title>
+    <Provider store={store}>
+      <div className="root">
+        <Head>
+          <title>Iegriez pasauli</title>
           <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      {!isActive && <Navbar />}
-      <AnimatePresence exitBeforeEnter={true}>
-        <Component
-          {...pageProps}
-          setIsActive={setIsActive}
-          isActive={isActive}
-        />
-      </AnimatePresence>
-    </div>
+        </Head>
+        {!isActive && <Navbar />}
+        <AnimatePresence exitBeforeEnter={true}>
+          <Component
+            {...pageProps}
+            setIsActive={setIsActive}
+            isActive={isActive}
+          />
+        </AnimatePresence>
+      </div>
+    </Provider>
   );
 }
 
