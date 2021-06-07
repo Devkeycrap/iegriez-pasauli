2c0268c7cf72c60d5d0a6d7797a7551e12df7781
Devkeycrap <ronalds.palacis@outlook.com>
2021-06-07 21:47:12 +0300

    Finished users endpoint

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from .views import UsersViewSet
 
 router = routers.DefaultRouter()
-router.register('api/users', UsersViewSet)
+router.register(r'api/users', UsersViewSet, 'users')
 
 urlpatterns = router.urls
