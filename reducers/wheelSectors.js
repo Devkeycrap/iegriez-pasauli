1ed2e6f1a1f66edc0c12489891786788cab10a4b
Devkeystuff <ronalds.palacis@outlook.com>
2021-07-06 16:17:39 +0300

    Temporarily removed whelSectors state redux

--- reducers/wheelSectors.js
+++ reducers/wheelSectors.js
@@ -1,23 +1,23 @@
-import axios from "axios";
+// import axios from "axios";
 
-import { REMOVE_SECTOR } from "../actions/types";
+// import { REMOVE_SECTOR } from "../actions/types";
 
-const initialState = {
-  sectors: await axios.get("http://localhost:8000/api/gameItems").then((res) =>
-    res.data.map((item) => ({
-      name: item.name,
-    }))
-  ),
-};
+// const initialState = {
+//   sectors: axios.get("http://localhost:8000/api/gameItems").then((res) =>
+//     res.data.map((item) => ({
+//       name: item.name,
+//     }))
+//   ),
+// };
 
-export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case REMOVE_SECTOR:
-      return {
-        ...state,
-        sectors: state.sectors.splice(action.payload, 1),
-      };
-    default:
-      return state;
-  }
-}
+// export default function reducer(state = initialState, action) {
+//   switch (action.type) {
+//     case REMOVE_SECTOR:
+//       return {
+//         ...state,
+//         sectors: state.sectors.splice(action.payload, 1),
+//       };
+//     default:
+//       return state;
+//   }
+// }
