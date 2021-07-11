bbe5ff47d2e05104e380f4207103c3cc5c557367
Devkeystuff <ronalds.palacis@outlook.com>
2021-07-11 13:14:50 +0300

    Points fix

--- reducers/points.js
+++ reducers/points.js
@@ -9,9 +9,7 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_POINTS:
-      return {
-        ...state,
-      };
+      return action.payload;
     default:
       return state;
   }
