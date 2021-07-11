a62d3cce8a0dc0af3459f84ec4297e1c8ad90286
Devkeystuff <ronalds.palacis@outlook.com>
2021-07-11 15:04:36 +0300

    End stage redesign

--- components/End.component.tsx
+++ components/End.component.tsx
@@ -54,49 +54,44 @@ export function End({
   };
 
   return (
-    <div className={styles.end}>
-      <div className={styles.container}>
-        <h2>Spēle beigusies!</h2>
+    <div className={styles.container}>
+      <h2>Spēle beigusies!</h2>
+      <h1>
+        Tu esi <span>{getResultLevel()}</span>!
+      </h1>
+      <h5>Tavi rezultāti</h5>
+      <div className={styles["result-table"]}>
+        <h3>Patiesība/meli</h3>
         <h4>
-          Tu esi <span>{getResultLevel()}</span>!
+          {points.quiz} <span>punkti</span>
         </h4>
-        <h5>Tavi rezultāti</h5>
-        <div className={styles["result-table"]}>
-          <h3>Patiesība/meli</h3>
-          <h4>{points.quiz}</h4>
-          <h3>Pasaules karte</h3>
-          <h4>{points.map}</h4>
-          <h3>Augošie vārdi</h3>
-          <h4>{points.words}</h4>
-        </div>
-        <div className={styles["btn-container"]}>
-          <Link href="/">
-            <button onClick={() => endGame()} className="btn btn-neutral">
-              Uz sākumu
-            </button>
-          </Link>
-          <Link href="/game">
-            <button onClick={() => restartGame()} className="btn btn-orange">
-              Spēlēt vēlreiz
-            </button>
-          </Link>
-        </div>
+        <h3>Pasaules karte </h3>
+        <h4>
+          {points.map} <span>punkti</span>
+        </h4>
+        <h3>Augošie vārdi</h3>
+        <h4>
+          {points.words} <span>punkti</span>
+        </h4>
       </div>
-      <div className={styles.leaderboard}>
-        <h2>Spēles tops</h2>
-        <div className={styles["leaderboard-container"]}>
-          {leaderboard &&
-            leaderboard.map((item: IUser, i) => (
-              <div
-                className={`${styles.user} ${i == 0 && styles.first} ${
-                  i == 1 && styles.second
-                } ${i == 3 && styles.third}`}
-              >
-                <h4>{item.username}</h4>
-                <h3>{item.points}</h3>
-              </div>
-            ))}
-        </div>
+      <div className={styles["btn-container"]}>
+        <Link href="/">
+          <button onClick={() => endGame()} className="btn btn-neutral">
+            Uz sākumu
+          </button>
+        </Link>
+        <Link href="/game">
+          <button onClick={() => restartGame()} className="btn btn-orange">
+            Spēlēt vēlreiz
+          </button>
+        </Link>
+      </div>
+      <div className={styles["confetti-container"]}>
+        {Array.from(Array(149).keys()).map((item) => (
+          <div
+            className={`${styles.confetti} ${styles[`confetti-${item}`]}`}
+          ></div>
+        ))}
       </div>
     </div>
   );
