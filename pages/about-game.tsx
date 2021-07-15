89a43eabfb71ed8a4d725fe42193cde88c831594
kristaps <kkrisapss.m@gmail.com>
2021-07-15 20:09:47 +0300

    Frontpage re-design- 'about-game'

--- pages/about-game.tsx
+++ pages/about-game.tsx
@@ -1,95 +1,154 @@
 // Styles & animations
 import styles from "../styles/aboutgame.module.scss";
-import { motion } from "framer-motion";
 import { fadeInUp, stagger } from "../models/animations/animations";
+import { motion } from "framer-motion";
 
-// About game page
-export default function AboutGame() {
+// Team page
+export default function AboutUs() {
+
   return (
     <motion.div
-      exit={{ opacity: 0 }}
-      initial="initial"
-      animate="animate"
-      className={styles["about-game"]}
+    exit={{ opacity: 0 }}
+    initial="initial"
+    animate="animate"
+    className={styles["about-game"]}
     >
-      <h4>Par spēli</h4>
-      <motion.div variants={stagger} className={styles["page-wrapper"]}>
-        <motion.div variants={fadeInUp} className={styles.level}>
-          <div className={styles["title-wrapper"]}>
-            <h1>
-              <span>0.</span>
-            </h1>
-            <h5>Laimes rats</h5>
-          </div>
-          <div className={styles["paragraph-wrapper"]}>
-            <p>
-              Iegriez ratu un uzzini spēles tēmu: banāni, hamburgers, austiņas,
-              skrituļdēlis vai T-krekls.
-            </p>
-          </div>
+      <h4>Projekts</h4>
+      <div className={styles["page-wrapper"]}>
+
+        <motion.div 
+        variants={fadeInUp} 
+        className={styles.project}
+        >
+          <p>
+            Spēle "RITenis" ir par Tevi pasaulē - par to, kā domāt un dzīvot videi draudzīgāk; par to, kas notiek pasaulē un kā Tu vari to ietekmēt; par to, kā lietas var izmantot atkārtoti, remontēt, atjaunot, pārstrādāt, lai tās kalpotu ilgāk, tādējādi taupot dabas resursus. Šī spēle mainīs Tavu skatu uz daudzām lietām. Iesaisties aprites ekonomikā - esi RITenis, nevis PĀRTenis.
+          </p>
         </motion.div>
-        <motion.div variants={fadeInUp} className={styles.level}>
-          <div className={styles["title-wrapper"]}>
-            <h1>
-              <span>1.</span>
-            </h1>
-            <h5>Izvērtē apgalvojumus!</h5>
-          </div>
-          <div className={styles["paragraph-wrapper"]}>
-            <p>
-              Spēles pirmajā līmenī izvērtē dotos apgalvojumus. Par katru
-              pareizu atbildi saņem 1 punktus.
-            </p>
-          </div>
-        </motion.div>
-        <motion.div variants={fadeInUp} className={styles.level}>
-          <div className={styles["title-wrapper"]}>
-            <h1>
-              <span>2.</span>
-            </h1>
-            <h5>Karte</h5>
-          </div>
-          <div className={styles["paragraph-wrapper"]}>
-            <p>
-              Spēles otrajā līmenī izvēlies pareizo vārda vai frāzes
-              skaidrojumu. Par katru pareizu atbildi saņem 1 punktus.
-            </p>
-          </div>
-        </motion.div>
-        <motion.div variants={fadeInUp} className={styles.level}>
-          <div className={styles["title-wrapper"]}>
-            <h1>
-              <span>3.</span>
-            </h1>
-            <h5>Augošie vārdi</h5>
-          </div>
-          <div className={styles["paragraph-wrapper"]}>
-            <p>
-              Spēles trešajā līmenī klikšķini uz vārdiem, kas saistīti ar
-              aprites ekonomiku. Dotais laiks - 15 sekundes. Par katru pareizi
-              izvēlētu vārdu 0.25 punkti.
-            </p>
-          </div>
-        </motion.div>
-        <div className={styles.restart}>
-          <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="redo-alt"
-            className={styles["icon-restart"]}
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
+
+        <motion.div 
+        variants={fadeInUp} 
+        className={styles.creators}
+        >
+          <p>
+            Spēli digitalizējuši Kristaps Mihelsons un Ronalds Palacis
+          </p>
+          <p>
+            no Ventspils Valsts 1. ģimnāzijas.
+          </p>
+
+          <div 
+          className={styles["wrapper"]}
           >
-            <path
-              fill="#9aca3c"
-              d="M256.455 8c66.269.119 126.437 26.233 170.859 68.685l35.715-35.715C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.75c-30.864-28.899-70.801-44.907-113.23-45.273-92.398-.798-170.283 73.977-169.484 169.442C88.764 348.009 162.184 424 256 424c41.127 0 79.997-14.678 110.629-41.556 4.743-4.161 11.906-3.908 16.368.553l39.662 39.662c4.872 4.872 4.631 12.815-.482 17.433C378.202 479.813 319.926 504 256 504 119.034 504 8.001 392.967 8 256.002 7.999 119.193 119.646 7.755 256.455 8z"
-            ></path>
-          </svg>
-          <div>Spēles noslēgumā uzzini vai esi RITenis vai PĀRTenis</div>
-        </div>
-      </motion.div>
+            <div 
+            className={styles.sm}
+            >
+              <h2>
+                Kristaps Mihelsons
+              </h2>
+
+              <div>      
+                  <h1>
+                    <a 
+                    href="https://www.instagram.com/kristaps0s/"
+                    >
+                      kristaps0s
+                    </a>
+                  </h1>
+                  <h1>
+                    <a 
+                    href="https://github.com/kristaps0s/"
+                    >
+                      kristaps0s
+                    </a>
+                  </h1>
+              </div>
+            </div>
+
+            <div 
+            className={styles.sm}
+            >
+              <h2>
+                Ronalds Palacis
+              </h2>
+                <div>
+                  <h1>
+                    <a href="https://www.instagram.com/rwnalds/">
+                      rwnalds
+                    </a>
+                  </h1>
+                  <h1>
+                    <a 
+                    href="https://github.com/devkeystuff/"
+                    >
+                      devkeystuff
+                    </a>
+                  </h1>
+                </div>
+            </div>
+          </div>
+        </motion.div>
+
+        <motion.div
+        variants={fadeInUp}
+        className={styles.instructions}>
+          <div>
+            <div>
+              <h1>
+                1.
+              </h1>
+              <p>
+                Iegriez ratu un uzzini spēles tēmu: banāni hamburgers skrituļdēlis vai T-krekls.
+              </p>
+            </div>
+            <div>
+              <h1>
+                2.
+              </h1>
+              <p>
+                Spēles pirmajā līmenī izvērtē dotos apgalvojumus. Par katru pareizu atbildi saņem 5 punktus. (arī uz ekrāna nevajag PATIESĪBA vai MELI - tu jābūt Izvērtē apgalvojumus! - kā bija arī manis sūtītajā failā)
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h1>
+                3.
+              </h1>
+              <p>
+                Spēles otrajā līmenī izvēlies pareizo vārda vai frāzes skaidrojumu. Par katru pareizu atbildi saņem 5 punktus. (šī daļa man neiet, nevaru komentēt tāpēc neko tālāk)
+              </p>
+            </div>
+            <div>
+              <h1>
+                4.
+              </h1>
+              <p>
+                Spēles trešajā līmenī klikšķini uz vārdiem, kas saistīti ar aprites ekonomiku. Dotais laiks - 15 sekundes. Par katru pareizi izvēlētu vārdu 0.5 punkti.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <svg 
+              aria-hidden="true" 
+              focusable="false" 
+              data-prefix="fas" 
+              data-icon="trophy" 
+              class="svg-inline--fa fa-trophy fa-w-18" 
+              role="img" xmlns="http://www.w3.org/2000/svg" 
+              viewBox="0 0 576 512"
+              >
+                <path
+                fill="#9ACA3C" 
+                d="M552 64H448V24c0-13.3-10.7-24-24-24H152c-13.3 0-24 10.7-24 24v40H24C10.7 64 0 74.7 0 88v56c0 35.7 22.5 72.4 61.9 100.7 31.5 22.7 69.8 37.1 110 41.7C203.3 338.5 240 360 240 360v72h-48c-35.3 0-64 20.7-64 56v12c0 6.6 5.4 12 12 12h296c6.6 0 12-5.4 12-12v-12c0-35.3-28.7-56-64-56h-48v-72s36.7-21.5 68.1-73.6c40.3-4.6 78.6-19 110-41.7 39.3-28.3 61.9-65 61.9-100.7V88c0-13.3-10.7-24-24-24zM99.3 192.8C74.9 175.2 64 155.6 64 144v-16h64.2c1 32.6 5.8 61.2 12.8 86.2-15.1-5.2-29.2-12.4-41.7-21.4zM512 144c0 16.1-17.7 36.1-35.3 48.8-12.5 9-26.7 16.2-41.8 21.4 7-25 11.8-53.6 12.8-86.2H512v16z"
+                /></svg>
+                <p>
+                  Spēles noslēgumā uzzini vai esi
+                </p>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </motion.div>
   );
 }
