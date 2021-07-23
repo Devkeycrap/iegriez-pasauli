3e5943668b17a7c08d4b70a9955380cc38273daa
kristaps <kkrisapss.m@gmail.com>
2021-07-23 19:20:50 +0300

    Page '404' rework

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,12 +3,11 @@ import styles from "../styles/fourohfour.module.scss";
 export default function FourOhFour() {
   return (
     <div className={styles.error}>
+      <h4>:(</h4>
       <div>
-        
         <h2>404</h2>
         <h1>Tas nebija pārāk</h1>
         <h1>labi ekonomikai</h1>
-        {/* <div className={styles.sad}>:(</div> */}
       </div>
     </div>
   );
