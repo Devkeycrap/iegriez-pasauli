611060657a28ecae8465af3dcd94bddd604e1b10
Ronalds Palacis <ronalds.palacis@outlook.com>
2021-08-19 19:00:05 +0300

    Debug

--- actions/points.js
+++ actions/points.js
@@ -6,6 +6,7 @@ const emitter = new EventEmitter();
 emitter.on("POINTS_UPDATE", (points) => setPoints(points));
 // Set game points
 export const setPoints = (points) => (dispatch) => {
+  console.log(points);
   dispatch({
     type: SET_POINTS,
     payload: points,
